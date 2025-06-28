9e9e95ab0e7b4051dc4efaa592f910e39348393e
H10J-cyber <hridhaanjain123@gmail.com>
2025-06-28 12:29:17 +0530

    Initial commit: Added Task 1, Task 2 scripts and README

---
+++ Task 1.py
@@ -0,0 +1,7 @@
+# Task 1: Check if a Number is Even or Odd
+number = int(input("Enter an integer: "))
+
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
---
+++ task 2.py
@@ -0,0 +1,7 @@
+# Calculate the sum of numbers from 1 to 50
+total = 0  # Initialize sum variable
+
+for num in range(1, 51):  # Loop from 1 to 50 (inclusive)
+    total += num  # Add each number to the total
+
+print("The sum of numbers from 1 to 50 is:", total)
